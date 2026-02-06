91a6508fcf112e66ea3be383c7f91e3d4cb2d08d
govardhandegala <govardhandegala008@gmail.com>
2026-02-06 09:56:44 +0530

    worked on home

--- wavepower/src/Pages/HomePage.jsx
+++ wavepower/src/Pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { HomeBanner } from '../Components/Home/HomeBanner'
-
+import BecameAPartner from '../Components/Home/BecameAPartner'
+import './home.css'
+import RealStories from '../Components/Home/RealStories'
+import TrustedBy from '../Components/Home/TrustedBy'
 export const HomePage = () => {
   return (
     <>
     <HomeBanner/>
+    < TrustedBy/>
+    <RealStories/>
+    <BecameAPartner/>
     </>
   )
 }
